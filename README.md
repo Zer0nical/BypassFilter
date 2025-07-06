60a415c92bbd9f470a515c7182f96812ac09c767
Zer0Nical <savesallgames@gmail.com>
2025-07-06 13:00:30 +0300

    TEST:

---
+++ README.md
@@ -0,0 +1,2 @@
+![info] somethiing
+> markdown test
---
+++ src/cpp-src/printLogoConsole.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    std::cout << "\033[33m";
+    std::cout << "                                 \n";
+    std::cout << "B                 %%%%%%%%%%%%%%% \n";
+    std::cout << "Y                %%%%%%%%%%%%%%%% \n";
+    std::cout << "P                %%%%%%%%%%%%%%% \n";
+    std::cout << "A               %%%%%%%%%%%%%%%  \n";
+    std::cout << "S              %%%%%%%%%%%%%%%   \n";
+    std::cout << "S              %%%%%%%%%%%%%%     \n";
+    std::cout << "-             %%%%%%%%%%%%%%      \n";
+    std::cout << "-             %%%%%%%%%%%%%       \n";
+    std::cout << "-            %%%%%%%%%%%%%%%%%%%%%\n";
+    std::cout << "-           %%%%%%%%%%%%%%%%%%%   \n";
+    std::cout << "-           %%%%%%%%%%%%%%%%%%    \n";
+    std::cout << "-                   %%%%%%%%%     \n";
+    std::cout << "-                   %%%%%%%       \n";
+    std::cout << "-                  %%%%%%%        \n";
+    std::cout << "F                  %%%%%%         \n";
+    std::cout << "I                 %%%%%           \n";
+    std::cout << "L                %%%%%            \n";
+    std::cout << "T                %%%%             \n";
+    std::cout << "E                %%               \n";
+    std::cout << "R               %%                \n";
+    std::cout << "\033[0m";
+    return 0;
+}
